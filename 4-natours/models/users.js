34b591b57c4fecf8263f527eed63d4cdd726fc09
el-yisus-dev <antoniodeveloper28@gmail.com>
2026-04-15 23:20:23 -0600

    feat(User): implement the User Schema

---
+++ 4-natours/models/users.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'The name is required'],
+    min: [1, 'You need a more larger name'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'The lastname is required'],
+    min: [1, 'You need a more larger name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'The lastname is required'],
+    unique: [true, 'This email is already register'],
+  },
+  username: {
+    type: String,
+    required: [true, 'The username is required'],
+    unique: [true, 'This username is already register'],
+  },
+});
+
+exports.User = model('Users', userSchema);
